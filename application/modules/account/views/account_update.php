e339b147a3f21f3bed02e86d115f6776dc16472f
fan_kevin <rong9x@gmail.com>
2014-09-27 10:28:41 +0700

    update view

--- application/modules/account/views/account_update.php
+++ application/modules/account/views/account_update.php
@@ -8,39 +8,41 @@
  * @var $role_tree string Html format of role tree nodes
  */
 
-echo "<link rel='stylesheet' href='" . base_url() . "assets/third_party/jstree/themes/default/style.min.css'/>";
-echo "<script src='" . base_url() . "assets/js/jquery.js'></script>";
-echo "<script src='" . base_url() . "assets/third_party/jstree/jstree.min.js'></script>";
+echo "<link rel='stylesheet' href='" . base_url() . "dist/themes/default/style.min.css'/>";
+echo "<script src='" . base_url() . "dist/libs/jquery.js'></script>";
+echo "<script src='" . base_url() . "dist/jstree.min.js'></script>";
 echo "<script src='" . base_url() . "application/modules/account/views/js/role_tree.js'></script>";
 
 echo form_open('account/account_controller/update');
 echo form_hidden('id', $info['id']);
 echo form_fieldset('Information');
-echo '<p>';
-echo form_label('Account Name');
-echo form_input('account_name', $info['account_name']);
+echo '<div class="form-group">';
+echo form_label('Username');
+echo form_input('account_name', $info['account_name'],"class='form-control'","id=''");
 echo form_error('account_name');
-echo '</p>';
-echo '<p>';
+echo '</div>';
+echo '<div class="form-group">';
 echo form_label('Staff Name');
-echo form_input('staff_name', $info['staff_name']);
+echo form_input('staff_name', $info['staff_name'],"class='form-control'","id=''");
 echo form_error('staff_name');
-echo '</p>';
-echo '<p>';
+echo '</div>';
+echo '<div class="form-group">';
 echo form_label('Password');
-echo form_password('password', $info['password']);
+echo form_password('password', $info['password'],"class='form-control'","id=''");
 echo form_error('password');
-echo '</p>';
-echo '<p>';
+echo '</div>';
+echo '<div class="form-group">';
 echo form_label('Address');
-echo form_input('address', $info['address']);
-
+echo form_input('address', $info['address'],"class='form-control'","id=''");
+echo '</div>';
 echo form_fieldset('Assigned roles');
 echo "<div id='role_tree'>";
 print $role_tree;
 echo "</div>";
 echo form_fieldset_close();
 
-echo form_button('btn_update', 'Update', "id='btn_update'");
+echo form_button('btn_update', 'Update',"class='btn btn-success'");echo '      ';
+echo form_button('btn_cancel', 'Cancel',"class='btn btn-success'");
+
 
 
